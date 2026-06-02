70c8046c27a43772b3ac894788c797f32d7442c6
Thejairex <yairjesus777@gmail.com>
2026-06-02 17:56:35 -0300

    refactor(migrations): remove old migration files and add new ones for conversations, messages, and participants

--- database/migrations/2026_05_27_203839_create_job_aplications_table.php
+++ database/migrations/2026_05_27_203839_create_job_aplications_table.php
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('job_aplications');
+        Schema::dropIfExists('job_applications');
     }
 };
